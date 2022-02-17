3fe9cc4b36b0ff29e9e6281c3dcf8cc8b88dea93
Shalah <zoungranaala@gmail.com>
2022-02-17 15:58:18 -0500

    Added a main js file as well as a 404 page and a route folder

---
+++ develop/server.js
@@ -0,0 +1,6 @@
+const express = ('express')
+const fs = ('fs')
+const path = ('path')
+
+const app = express();
+const PORT = process.env.port || 3001;   // Find any port for my work or give me port 3001

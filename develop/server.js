da2b3f9ddcc5bbc2f01709d87d5a1035164fd5a1
Shalah <zoungranaala@gmail.com>
2022-02-17 17:04:01 -0500

    Added more routes: 404page, note and homepage.

--- develop/server.js
+++ develop/server.js
@@ -5,7 +5,27 @@ const path = require('path')
 const app = express();
 const PORT = process.env.port || 3001;   // Find any port for my work or give me port 3001
 
-// This listen for any port
-app.listen(PORT, () => 
-    console.log (`App listening at http://localhost:${PORT}`)
+// Middleware
+app.use(express.json())
+
+//Route for homepage
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// Route for the note.html 
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
 )
+
+// Route for the 404 page. Always at the END of the page
+// Telling the system to go there if no port corresponds to what the user clicked.
+// "*" means wildcard route. The ones that dont exist.
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirnmane, '/public/page/404.html'))
+);
+
+// Event listener on the port
+app.listen(PORT, () => 
+    console.log (`App listening at http://localhost:${PORT}`) 
+);

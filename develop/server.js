6b33259452bf36e7096550db72ff842ed9e0a077
Shalah <zoungranaala@gmail.com>
2022-02-17 16:10:13 -0500

    Added an event listener on the port

--- develop/server.js
+++ develop/server.js
@@ -1,6 +1,11 @@
-const express = ('express')
-const fs = ('fs')
-const path = ('path')
+const express = require('express')
+const fs = require('fs')
+const path = require('path')
 
 const app = express();
 const PORT = process.env.port || 3001;   // Find any port for my work or give me port 3001
+
+// This listen for any port
+app.listen(PORT, () => 
+    console.log (`App listening at http://localhost:${PORT}`)
+)

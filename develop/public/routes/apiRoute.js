6e2edfe5cdccb387fcea7183a1d156bd26604a2e
Shalah <zoungranaala@gmail.com>
2022-02-19 19:44:37 -0500

    Reprogram the whole routes. Everything works excpet the delete command

--- develop/public/routes/apiRoute.js
+++ develop/public/routes/apiRoute.js
@@ -1,64 +1,64 @@
 // Dependencies
-const express = require('express')
-const app = express();
-const fs = require("fs");
+// const express = require('express')
+// const app = express();
+// const fs = require("fs");
 
-const ar = require('express').Router();
+// const ar = require('express').Router();
 
-const { v4: uuidv4 } = require('uuid'); // Importing this npm package for id
-
-
-// This is for the get (Read) request
-app.get("/api/notes", (req, res) => {
-
-    console.log(`\nGetting get reqs \n`);
-
-    let data = JSON.parse(fs.readFileSync(`./db/db.json`, `utf8`)); // this reads the json file
-
-    console.log(`\nReturning data : ` + JSON.stringify(data));
-
-    res.json(data); // this sends data
-});
+// const { v4: uuidv4 } = require('uuid'); // Importing this npm package for id
 
 
-// This is for the post (write) request
-app.post("/api/notes", (req, res) => {
+// // This is for the get (Read) request
+// app.get("/api/notes", (req, res) => {
 
-    // Extracted new note from req body.  
-    const newNote = req.body;
+//     console.log(`\nGetting get reqs \n`);
 
-    console.log(`\nNew Note : ` + JSON.stringify(newNote));
+//     let data = JSON.parse(fs.readFileSync(`./db/db.json`, `utf8`)); // this reads the json file
 
-    newNote.ID = uuidv4();  //Using the npm package uuidv4, an id is assigned to the note
+//     console.log(`\nReturning data : ` + JSON.stringify(data));
 
-    let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8")); //This reads the data
-
-    data.push(newNote); // This adds note to existing ones
-
-    fs.writeFileSync(`./db/db.json`, JSON.stringify(data));
-
-    console.log(`\n New note was successfully added to the file.`);
-
-    res.json(data);
-});
+//     res.json(data); // this sends data
+// });
 
 
-// This is for the DELETE (delete) request
-app.delete("/api/notes/:id", (req, res) => {
+// // This is for the post (write) request
+// app.post("/api/notes", (req, res) => {
 
-    let noteID = req.params.id.toString(); // This gets the id to delete the right file
+//     // Extracted new note from req body.  
+//     const newNote = req.body;
 
-    console.log(`\n Deleting note: ${noteID}`);
+//     console.log(`\nNew Note : ` + JSON.stringify(newNote));
 
-    let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8")); //This reads the data
+//     newNote.ID = uuidv4();  //Using the npm package uuidv4, an id is assigned to the note
 
-    const newData = data.filter (note => note.ID.toString() !== noteID);
+//     let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8")); //This reads the data
 
-    fs.writeFileSync('./db/db.json', JSON.stringify(newData));
+//     data.push(newNote); // This adds note to existing ones
 
-    console.log(`\n Note was successfully deleted using this ID: ${noteID}`);
+//     fs.writeFileSync(`./db/db.json`, JSON.stringify(data));
 
-    res.json(newData);
-});
+//     console.log(`\n New note was successfully added to the file.`);
 
-module.exports = ar;
+//     res.json(data);
+// });
+
+
+// // This is for the DELETE (delete) request
+// app.delete("/api/notes/:ID", (req, res) => {
+
+//     let noteID = req.params.ID.toString(); // This gets the id to delete the right file
+
+//     console.log(`\n Deleting note: ${noteID}`);
+
+//     let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8")); //This reads the data
+
+//     const newData = data.filter (note => note.ID.toString() !== noteID);
+
+//     fs.writeFileSync('./db/db.json', JSON.stringify(newData));
+
+//     console.log(`\n Note was successfully deleted using this ID: ${noteID}`);
+
+//     res.json(newData);
+// });
+
+// module.exports = ar;
--- develop/server.js
+++ develop/server.js
@@ -17,7 +17,7 @@ app.use(express.json())
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 
-//app.use('/api', api);
+
 
 // Route for the notes
 app.get('/notes', (req, res) => 
@@ -29,90 +29,49 @@ app.get('/index', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-// This is for the get (Read) request
-app.get("/api/notes", (req, res) => {
 
-    console.log(`\nGetting get reqs`);
+// Try
+app.get('/api/notes', (req, res) => 
+    res.sendFile(path.join(__dirname, './db/db.json'))
+);
 
-    let data = JSON.parse(fs.readFileSync(`./db/db.json`, `utf8`)); // this reads the json file
-
-    //console.log(`\nReturning data : ` + JSON.stringify(data));
-
-    res.json(data); // this sends data
-});
-
-// This is for the post (write) request
-app.post("/api/notes", (req, res) => {
-
-    // Extracted new note from req body.  
-    const newNote = req.body;
-
-    console.log(`\nNew Note : ` + JSON.stringify(newNote));
-
-    newNote.ID = uuidv4();  //Using the npm package uuidv4, an id is assigned to the note
-
-    let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8")); //This reads the data
-
-    data.push(newNote); // This adds note to existing ones
-
-    fs.writeFileSync(`./db/db.json`, JSON.stringify(data));
-
-    console.log(`\n New note was successfully added to the file.`);
-
-    res.json(data);
+app.post('/api/notes', (req, res) => {
+    let newNotes = {
+        id: uuidv4(),
+        title: req.body.title,
+        text: req.body.text,
+    };
+    let pastNote = JSON. parse(fs. readFileSync(path.join(__dirname,"./db/db.json"),"utf-8"))
+    pastNote. push(newNotes)
+    fs.writeFileSync("./db/db.json", JSON.stringify(pastNote))
+    res. json(pastNote)
 });
 
 // This is for the DELETE (delete) request
-// app.delete("/api/notes/:id", (req, res) => {
+app.delete("/api/notes/:ID", (req, res) => {
 
-//     let noteID = req.params.id.toString(); // This gets the id to delete the right file
+    let noteID = req.params.ID.toString(); // This gets the id to delete the right file
 
-//     console.log(`\n Deleting note: ${noteID}`);
+    console.log(`\n Deleting note: ${noteID}`);
 
-//     let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8")); //This reads the data
+    let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8")); //This reads the data
 
-//     const newData = data.filter (note => note.ID.toString() !== noteID);
+    const newData = data.filter (note => note.ID.toString() !== noteID);
 
-//     fs.writeFileSync('./db/db.json', JSON.stringify(newData));
-
-//     console.log(`\n Note was successfully deleted using this ID: ${noteID}`);
-
-//     res.json(newData);
-// });
-
-
-
-// Try
-
-app.delete("/api/notes/:id", (request, response) => {
-
-    // Fetched id to delete
-    let noteId = request.params.id.toString();
-    
-    console.log(`\n\nDELETE note request for noteId: ${noteId}`);
-
-    // Read data from 'db.json' file
-    let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
-
-    // filter data to get notes except the one to delete
-    const newData = data.filter( note => note.id.toString() !== noteId );
-
-    // Write new data to 'db.json' file
     fs.writeFileSync('./db/db.json', JSON.stringify(newData));
-    
-    console.log(`\nSuccessfully deleted note with id : ${noteId}`);
 
-    // Send response
-    response.json(newData);
+    console.log(`\n Note was successfully deleted using this ID: ${noteID}`);
+
+    res.json(newData);
 });
 
-//Try
 
 
 
 
 
 
+ 
 
 
 
